d585f71a580250ba1df332842a3e07f703f10b59
datahack007 <tarunreddyp94@gmail.com>
2020-07-03 17:50:16 -0500

    Commitment

---
+++ easy/count_and_say.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def countAndSay(self, n):
+        #111221
+        if n == 1:
+           return "1"
+        prev = self.countAndSay(n-1)
+        res =""
+        count = 1
+        for i in range(len(prev)):
+            if i==len(prev)-1 or prev[i] != prev[i+1]:
+                res += str(count) + prev[i]
+                count = 1
+            else:
+                count += 1
+        return res
+        
+
+if __name__ == "__main__":
+    a = Solution()
+    x = a.countAndSay(2)
+    print(x)
+    pass
