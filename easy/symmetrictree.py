0614148677e8e83b2559ce1b79ccb86a9b55b2fa
datahack007 <tarunreddyp94@gmail.com>
2020-07-05 01:28:09 -0500

    you got this

---
+++ easy/symmetrictree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if root is None:
+            return True
+        return self.ismirror(root.left,root.right)
+    def ismirror(self,leftroot,rightroot):
+        if leftroot and rightroot :
+            return leftroot.val == rightroot.val and self.ismirror(leftroot.left,rightroot.right) and self.ismirror(leftroot.right,rightroot.left)
+        return leftroot == rightroot
+
+    
+        # while self.left ==self.right:
+        #     if root.left.left == root.right.right and root.left.right == root.right.left:
+        #         return True
+        #     else:
+        #         return False
+
+
+        
